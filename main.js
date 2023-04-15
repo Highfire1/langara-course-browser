b6b5792131b5683e23e4695a25b6721c7484b899
Anderson T <tseng.andersonn@gmail.com>
2023-04-14 19:02:44 -0700

    more data (sorry git lmao)

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
 async function fetchdata() {
-    const response = await fetch("courseInfo.json");
+    const response = await fetch("allInfo.json");
     const jsonData = await response.json();
     return jsonData
 }
+
+
+document.addEventListener('DOMContentLoaded', async function() {
+
+    if (this.getElementById("courseTable").hasChildNodes()) {
+        return
+    }
+
+    data = await fetchdata()
+    data = data.courses
+
+    //data = data.slice(0, 5)
+
+
+    list = this.getElementById("courseTable")
+
+    for (const course in data) {
+        list.appendChild(generateHTML(data[course]))
+    }
+
+    this.getElementById("searchResults").textContent = `Currently displaying ${data.length} courses.`
+
+})
+
+
+
+
+
+
+
+
+
 function generateHTML(object) {
 
     html = ""
@@ -22,6 +54,12 @@ function generateHTML(object) {
     rpt = ""
     if (object.rpt_limit != null)
         rpt = object.rpt_limit
+    
+    let avail_color = "green"
+    if (object.availability == "discontinued") 
+        avail_color = "red"
+    if (object.availability == "unknown")
+        avail_color = "yellow"
 
     html += `
     <h3>${object.subject} ${object.course_code} ${object.title} ${hours}</h3>
@@ -40,15 +78,12 @@ function generateHTML(object) {
             <th>Repeat Limit:</th> 
             <td>${rpt}</td>
         </tr>
-        <tr>
+        <tr class=${avail_color}>
             <th>Availability:</th>
             <td>${object.availability}</td>
         </tr>
     </table>
     `
-    sems = object.prev_offered.join(", ")
-    html += `<p>Previously offered : ${sems}.</p>`
-
 
     if (object.attributes === null)
         html += "<p><b>No course attributes available.</b></p>"
@@ -65,41 +100,80 @@ function generateHTML(object) {
                 <td>${object.attributes["SOC"]}</td>
                 <td>${object.attributes["UT"]}</td>
         </table>
-        </br>
         `
     }
+
     if (object.transfer.length == 0)
         html += "<p><b>No transfer agreements found.</b></p>"
     else {
-        html += `<table><th>Course</th><th>Destination</th><th>Credit</th><th>Start/End</th>`
-        for (t of object.transfer) {
-            c = ""
-            if (t.effective_end != "present") 
-                c = "hidden"
+        html += `<table class="transferTable"> 
+        <th>Course</th><th>Destination</th><th>Credit</th><th>Start/End</th>
+        `
 
+        for (t of object.transfer) {
+
+            classes = ""
+            if (t.effective_end != "present") 
+                classes += "hidden "
+
+            if (t.credit == "No credit")
+                classes += "red "
+            
+            //if (t.credit != undefined)
+            //    console.log(t.credit.split("(").at(-1).split(")").at(0))
+
+            // yellow on ind assessment or if you only get partial credits for transfer
+            if (t.credit == "Individual assessment." || (t.credit != undefined && parseFloat(t.credit.split("(").at(-1).split(")").at(0)) < parseFloat(object.credits)))
+                classes += "yellow "
+            
             html += `
-                <tr class=${c}>
-                    <td>${t.subject} ${t.course_code}</td>
-                    <td>${t.destination}</td>
+                <tr class=${classes} ${t.destination}>
+                    <td class="tablePriority">${t.subject} ${t.course_code}</td>
+                    <td class="tablePriority">${t.destination}</td>
                     <td>${t.credit}</td>
-                    <td>${t.effective_start} to ${t.effective_end}</td>
+                    <td class="tablePriority">${t.effective_start} to ${t.effective_end}</td>
                 </tr>
             `
         }
         html += `</table>`
     }
-    let x = `
-    <p>
-        Transfers as:
-        "subject": "ABST",
-        "course_code": 1100,
-        "source": "LANG",
-        "destination": "CAPU",
-        "credit": "CAPU HIST 209 (3)",
-        "effective_start": "May/03",
-        "effective_end": "present"
-    </p>
+
+    sems = object.prev_offered.join(", ")
+    html += `<p>Previously offered : ${sems}.</p>`
+
+    html += `
+    <table class="offeredTable"> 
+    <thead><th>Semester</th> <th>Seats</th> <th>Waitlist</th> <th>Days</th> <th>Time</th> <th>Room</th> <th>Type</th> <th>Instructor</th></thead>
     `
+
+    for (c of object.offered) {
+        html += `<tbody>`
+        let s = []
+        for (sch of c.schedule) {
+            s.push(`
+            <td class=mono>${sch.days}</td> 
+            <td class=mono>${sch.time}</td> 
+            <td class=mono>${sch.room}</td> 
+            <td class=mono>${sch.instructor}</td>  
+            <td class=mono>${sch.type}</td> `
+            )
+        }
+
+        html += `<tr>
+            <td rowspan="${s.length}">${c.yearsemester}</td>
+            <td rowspan="${s.length}">${c.seats}</td>
+            <td rowspan="${s.length}">${c.waitlist}</td>            
+            ${s[0]}
+            </tr>
+        `        
+        for (string of s.slice(1)) {
+            html += `<tr>${string}</tr>`
+        }
+        html += `</tbody>`
+    }
+
+    html += `</table>`
+    
 
 
     let temp = document.createElement('div')
@@ -109,20 +183,3 @@ function generateHTML(object) {
 
     return temp
 }
-
-
-document.addEventListener('DOMContentLoaded', async function() {
-    data = await fetchdata()
-    data = data.courses
-
-
-    list = this.getElementById("courseTable")
-
-    for (const course in data) {
-        list.appendChild(generateHTML(data[course]))
-    }
-
-    this.getElementById("searchResults").textContent = `Currently displaying ${data.length} courses.`
-
-})
-
